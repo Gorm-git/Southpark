c4db6db9de9bd4b4f3cf20032ad9c86c9a852b0c
Gorm-git <tneermark@gmail.com>
2023-05-08 09:39:45 +0200

    Okay

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ async function initApp() {
   const characterSheet = await getPosts(
     "https://cederdorff.github.io/dat-js/05-data/southpark.json"
   );
-  showPosts(characterSheet);
+  characterSheet.forEach(showPost);
+  addInfoButtonClickHandlers();
 }
 
 //========= posts section =========//
@@ -27,78 +28,92 @@ function showPosts(listOfPosts) {
   }
 }
 
-function showPost(urlData, index) {
-  const html = /*html*/ `
-    <article id="post-${index}" class="grid-item">
+function showPost(urlData) {
+  const html = `
+    <article class="grid-item">
       <h2 class="name">${urlData.name}</h2>
       <img class="grid-image" src ="${urlData.image}" alt="${urlData.name}">
-      <button class="info">Details</button>
+      <button class="info" 
+              data-nickname="${urlData.nickname}" 
+              data-occupation="${urlData.occupation}" 
+              data-age="${urlData.age}" 
+              data-voiced_by="${urlData.voiced_by}" 
+              data-gender="${urlData.gender}" 
+              data-religion="${urlData.religion}" 
+              data-catch_phrase="${urlData.catch_phrase}" 
+              data-hair_color="${urlData.hair_color}" 
+              data-school_grade="${urlData.school_grade}" 
+              data-episodes="${urlData.episodes}" 
+              data-appearances="${urlData.appearances}" 
+              data-first_appearance="${urlData.first_appearance}">Details
+      </button>
     </article> 
   `;
   document.querySelector("#posts").insertAdjacentHTML("beforeend", html);
+}
 
-  const button = document.querySelector(`#post-${index} .info`);
-  const dialog = document.querySelector("#details-dialog");
-  const form = dialog.querySelector("form");
-
-  button.addEventListener("click", () => {
-    // find the post that matches the clicked button
-    const postId = button.parentNode.id;
-    const postIndex = parseInt(postId.substring(postId.indexOf("-") + 1));
-    const post = characterSheet[postIndex];
-
-    // set the values of the form inputs
-    form.querySelector("#name").value = post.name;
-    form.querySelector("#nickname").value = post.nickname;
-    form.querySelector("#image").src = post.image;
-    form.querySelector("#occupation").value = post.occupation.join(", ");
-    form.querySelector("#age").value = post.age;
-    form.querySelector("#voicedBy").value = post.voiced_by.join(", ");
-    form.querySelector("#gender").value = post.gender;
-    form.querySelector("#religion").value = post.religion;
-    form.querySelector("#catchPhrase").value = post.catchphrase;
-    form.querySelector("#hairColor").value = post.hair_color;
-    form.querySelector("#schoolGrade").value = post.school_grade;
-    form.querySelector("#episodes").value = post.episodes.join(", ");
-    form.querySelector("#appearances").value = post.appearances.join(", ");
-    form.querySelector("#firstAppearance").value = post.first_appearance;
-
-    // show the dialog
-    dialog.showModal();
+function addInfoButtonClickHandlers() {
+  const posts = document.querySelector("#posts");
+  posts.addEventListener("click", (event) => {
+    if (event.target.classList.contains("info")) {
+      const postElement = event.target.closest(".grid-item");
+      const urlData = {
+        name: postElement.querySelector(".name").textContent,
+        nickname: postElement.querySelector.textContent,
+        image: postElement.querySelector(".grid-image").src,
+        occupation: postElement.dataset.occupation,
+        age: postElement.dataset.age,
+        voiced_by: postElement.dataset.voiced_by,
+        gender: postElement.dataset.gender,
+        religion: postElement.dataset.religion,
+        catch_phrase: postElement.dataset.catch_phrase,
+        hair_color: postElement.dataset.hair_color,
+        school_grade: postElement.dataset.school_grade,
+        episodes: postElement.dataset.episodes,
+        appearances: postElement.dataset.appearances,
+        first_appearance: postElement.dataset.first_appearance,
+      };
+      displayCharacterInfo(urlData);
+    }
   });
 }
 
-function showDetails(character) {
-  const dialog = document.querySelector("#details-dialog");
-  const nameInput = dialog.querySelector("#name");
-  const nicknameInput = dialog.querySelector("#nickname");
-  const imageInput = dialog.querySelector("#image");
-  const occupationInput = dialog.querySelector("#occupation");
-  const ageInput = dialog.querySelector("#age");
-  const voicedByInput = dialog.querySelector("#voicedBy");
-  const genderInput = dialog.querySelector("#gender");
-  const religionInput = dialog.querySelector("#religion");
-  const catchPhraseInput = dialog.querySelector("#catchPhrase");
-  const hairColorInput = dialog.querySelector("#hairColor");
-  const schoolGradeInput = dialog.querySelector("#schoolGrade");
-  const episodesInput = dialog.querySelector("#episodes");
-  const appearancesInput = dialog.querySelector("#appearances");
-  const firstAppearanceInput = dialog.querySelector("#firstAppearance");
+function displayCharacterInfo(urlData) {
+  const characterInfo = {
+    name: urlData.name,
+    nickname: urlData.nickname,
+    image: urlData.image,
+    occupation: urlData.occupation,
+    age: urlData.age,
+    voicedBy: urlData.voiced_by,
+    gender: urlData.gender,
+    religion: urlData.religion,
+    catchPhrase: urlData.catch_phrase,
+    hairColor: urlData.hair_color,
+    schoolGrade: urlData.school_grade,
+    episodes: urlData.episodes,
+    appearances: urlData.appearances,
+    firstAppearance: urlData.first_appearance,
+  };
+  const dialog = document.querySelector("#dialog-info");
+  const form = dialog.querySelector(".form-body");
+  const image = form.querySelector("#image");
+  // Populate form fields with character information
+  form.name.value = characterInfo.name;
+  form.nickname.value = characterInfo.nickname;
+  image.src = characterInfo.image;
+  form.occupation.value = characterInfo.occupation;
+  form.age.value = characterInfo.age;
+  form.voicedBy.value = characterInfo.voicedBy;
+  form.gender.value = characterInfo.gender;
+  form.religion.value = characterInfo.religion;
+  form.catchPhrase.value = characterInfo.catchPhrase;
+  form.hairColor.value = characterInfo.hairColor;
+  form.schoolGrade.value = characterInfo.schoolGrade;
+  form.episodes.value = characterInfo.episodes;
+  form.appearances.value = characterInfo.appearances;
+  form.firstAppearance.value = characterInfo.firstAppearance;
 
-  nameInput.value = character.name;
-  nicknameInput.value = character.nickname;
-  imageInput.src = character.image;
-  occupationInput.value = character.occupation;
-  ageInput.value = character.age;
-  voicedByInput.value = character.voiced_by;
-  genderInput.value = character.gender;
-  religionInput.value = character.religion;
-  catchPhraseInput.value = character.catchphrase;
-  hairColorInput.value = character.hair_color;
-  schoolGradeInput.value = character.school_grade;
-  episodesInput.value = character.episodes.join(", ");
-  appearancesInput.value = character.appearances.join(", ");
-  firstAppearanceInput.value = character.first_appearance;
-
+  // Show the dialog
   dialog.showModal();
 }
